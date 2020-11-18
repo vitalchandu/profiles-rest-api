60f97ce956f2a73fe778736ab37b34666f3890ee
VitalChandu <vitalchandu13@gmail.com>
2020-11-18 15:27:57 +0530

    Added login API, Autherization

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path,include
 
 from rest_framework.routers import DefaultRouter
+
 from profiles_api import views
 
 router = DefaultRouter()
@@ -9,5 +10,6 @@ router.register('profile',views.UserProfileViewSet)
 
 urlpatterns = [
     path('hello-view/',views.HelloApiView.as_view()),
+    path('login',views.UserLoginApiView.as_view()),
     path('',include(router.urls))
 ]
